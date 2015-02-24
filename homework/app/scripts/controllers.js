f644919163835e8e0ef5548795b69397c14a039b
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-02-23 22:03:43 -0600

    pulling from tiny pizza, did not get filer or sorting

---
+++ homework/app/scripts/controllers.js
@@ -0,0 +1,23 @@
+angular.module('basic.controllers', ['basic.services'])
+.controller('BasicCtrl', function($scope, $http) {
+
+	$scope.filterBox = '';
+
+	var promise = $http.get('http://tiny-pizza-server.herokuapp.com/collections/fancy-table')
+		.success(function(response) {
+
+			$scope.states = [];
+			for(var i=0; i<response.length; i++) {
+
+				console.log(i);
+				console.log(response[i]);
+			
+					if(response[i].name && response[i].abbreviation) {
+						$scope.states.push(response[i]);
+						console.log($scope.states);
+					}
+			}
+
+		})
+
+});
